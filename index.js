7b8571a425e9f649b36c3a792d4717a5b648a331
tanjirman <tanjirmahbub07@gmail.com>
2026-07-05 17:31:56 +0600

    Integrate Express mongodb and other necessary package then adding the get,post and patch API for the seller dashboard

---
+++ index.js
@@ -0,0 +1,157 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 5000
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+ 
+
+
+
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const db = client.db('Resell-Hub');
+    const productCollection = db.collection('products');
+
+    app.get("/api/products/:email", async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    const result = await productCollection
+      .find({ sellerEmail: email })
+      .toArray();
+
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+});
+
+ app.post("/api/products", async (req, res) => {
+  try {
+    const {
+      title,
+      image,
+      description,
+      category,
+      condition,
+      price,
+      quantity,
+      sellerId,
+      sellerName,
+      sellerEmail,
+    } = req.body;
+
+    const product = {
+      title,
+      category,
+      condition,
+      price: Number(price),
+      quantity: Number(quantity),
+
+      image,
+      description,
+
+      sellerId,
+      sellerName,
+      sellerEmail,
+
+      createdAt: new Date(),
+      status: "available",
+    };
+
+    const result = await productCollection.insertOne(product);
+
+    res.status(201).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Failed to add product",
+    });
+  }
+});
+
+app.patch("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updateData = {
+      ...req.body,
+      price: Number(req.body.price),
+      quantity: Number(req.body.quantity),
+      updatedAt: new Date(),
+    };
+
+    const result = await productCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updateData }
+    );
+
+    const updatedProduct = await productCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    console.log("Mongo Result:", result);
+    console.log("Updated Product:", updatedProduct);
+
+    res.send(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Failed to update product",
+    });
+  }
+});
+
+  
+
+
+  
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
